cff7afd2298809b2c30e8f8e1ce1f81d3561e639
Arne Johannessen <noreply@arne.johannessen.de>
2023-08-06 17:54:20 +0200

    Add Neo4j temporal types

---
+++ lib/Neo4j/Types/Duration.pm
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+package Neo4j::Types::Duration;
+# ABSTRACT: Represents a Neo4j temporal duration value
+
+
+1;
---
+++ lib/Neo4j/Types/Generic/Duration.pm
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+
+package Neo4j::Types::Generic::Duration;
+# ABSTRACT: Generic representation of a Neo4j temporal duration value
+
+
+use parent 'Neo4j::Types::Duration';
+
+
+sub new {
+	my ($class, $params) = @_;
+	
+	$params->{$_} ||= 0 for qw( months days seconds nanoseconds );
+	return bless $params, __PACKAGE__;
+}
+
+
+sub months { shift->{months} }
+sub days { shift->{days} }
+sub seconds { shift->{seconds} }
+sub nanoseconds { shift->{nanoseconds} }
+
+
+1;
